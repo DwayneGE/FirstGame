a500a80bfe578abd1855b2172c160bb987c15fef
DwayneGE <dellsworth@gmx.de>
2017-08-12 18:24:05 +0200

    all

---
+++ src/com/tutorial/main/Player.java
@@ -0,0 +1,20 @@
+package com.tutorial.main;
+
+import java.awt.Graphics;
+
+public class Player extends GameObject {
+	
+	public Player (int x, int y, ID id) {
+		super(x, y, id);
+	}
+
+	public void tick() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void render(Graphics g) {
+		// TODO Auto-generated method stub
+		
+	}
+}
