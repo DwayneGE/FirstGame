33619421e20b4cdfa2fd48ea49ae42bf3c49b89c
DwayneGE <dellsworth@gmx.de>
2017-08-14 18:17:26 +0200

    Merge branch 'master' of https://github.com/DwayneGE/FirstGame

--- src/com/tutorial/main/Game.java
+++ src/com/tutorial/main/Game.java
@@ -1,6 +1,9 @@
 package com.tutorial.main;
 
 import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
 
 public class Game extends Canvas implements Runnable {
 
@@ -32,7 +35,12 @@ public class Game extends Canvas implements Runnable {
 	
 	public void run() {
 		long lastTime = System.nanoTime();
-		
+		double amountOfTicks = 60.0;
+		double ns = 1000000000 / amountOfTicks;
+		double delta = 0;
+		long timer = System.currentTimeMillis();
+		int frames = 0;
+		while(running) {
 			long now = System.nanoTime();
 			delta += (now - lastTime) / ns;
 			lastTime = now;
@@ -49,7 +57,26 @@ public class Game extends Canvas implements Runnable {
 				System.out.println("FPS: " + frames);
 				frames = 0;
 			}
-			
+		}
+		stop();
+	}
+	
+	public void tick() {
+		
+	}
+	
+	public void render() {
+		BufferStrategy bs = this.getBufferStrategy();
+		if(bs == null) {
+			this.createBufferStrategy(3);
+			return;
+		}
+		
+		Graphics g = bs.getDrawGraphics(); 
+		g.setColor(Color.black);
+		g.fillRect(0, 0, WIDTH, HEIGHT);
+		g.dispose();
+		bs.show();
 	}
 	
 	public static void main (String args[]) {
