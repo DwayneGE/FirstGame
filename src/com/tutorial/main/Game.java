feda93ab09222a05266b1f73af90d7a728c918ea
DwayneGE <dellsworth@gmx.de>
2017-08-20 14:04:22 +0200

    okay

--- src/com/tutorial/main/Game.java
+++ src/com/tutorial/main/Game.java
@@ -20,12 +20,14 @@ public class Game extends Canvas implements Runnable {
 	
 	public Game() {
 		handler = new Handler();
+		this.addKeyListener(new KeyInput(handler));
 		
 		new Window(WIDTH, HEIGHT, "Let's Build a Game!", this);
 		
 		r = new Random();
 		
 		handler.addObject(new Player(WIDTH/2-32, HEIGHT/2-32, ID.Player));
+		handler.addObject(new Player(WIDTH/2+64, HEIGHT/2-32, ID.Player2));
 	}
 	
 	
